5f74bd34ad7f9cb588919d50557a4535b9314364
natapn <natasha.panasyuk@gmail.com>
2022-12-30 01:05:34 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,75 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { data } from './data';
 import './App.css';
 
 function App() {
+  const [bag, setBag] = useState([]);
+  const [total, setTotal] = useState(0);
+  const [dress, setDress] = useState(0);
+  const { id, image, description, price } = data[dress];
+
+  const previousPerson = () => {
+    setDress(dress => {
+      dress--;
+      if (dress < 0) {
+        dress = data.length - 1;
+      }
+      return dress;
+    });
+  }
+
+  const nextPerson = () => {
+    setDress(dress => {
+      dress++;
+      if (dress > data.length - 1) {
+        dress = 0;
+      }
+      return dress;
+    });
+  }
+
+  const addToBag = () => {
+    setBag([...bag, { name: description, sum: price }]);
+    let newTotal = total + price;
+    setTotal(newTotal);
+  }
+
+  const removeItem = (item, sum) => {
+    let newBag = bag.filter((bag, index) => index !== item);
+    setBag(newBag);
+    let newTotal = total - sum;
+    setTotal(newTotal);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>DRESS SHOP</h1>
+      <div className="container">
+        <div className="catalog">
+          <div className="scroll">
+            <button className="btn" onClick={previousPerson}>{"<"}</button>
+            <img src={image} alt="dress" width="350px" height="520px" />
+            <button className="btn" onClick={nextPerson}>{">"}</button>
+          </div>
+          <h2>{id} - {description}</h2>
+          <p className="price">${price}</p>
+          <button className="add" onClick={addToBag}>ADD TO BAG</button>
+        </div>
+        <div className="bag">
+          <h3>shopping bag ({bag.length}) points:</h3>
+          {bag.map((element, index) => {
+            const { name, sum } = element;
+            return (
+              <div key={index} className="item">
+                <p className="name">{name}</p>
+                <p className="sum">${sum}</p>
+                <button className="delete" onClick={() => removeItem(index, sum)}>✕</button>
+              </div>
+            )
+          })}
+          <h3>total: ${total}</h3>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,38 @@
+export const data = [
+
+    {
+        id: 1,
+        image: "https://images.unsplash.com/photo-1568252542512-9fe8fe9c87bb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=719&q=80",
+        description: "dress for photoshoot",
+        price: 200
+    },
+
+    {
+        id: 2,
+        image: "https://images.unsplash.com/photo-1592020051126-c240d2aaa98b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=686&q=80",
+        description: "summer dress",
+        price: 130
+    },
+
+    {
+        id: 3,
+        image: "https://images.unsplash.com/photo-1582533561751-ef6f6ab93a2e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=655&q=80",
+        description: "party dress",
+        price: 180
+    },
+
+    {
+        id: 4,
+        image: "https://images.unsplash.com/photo-1635761331499-e52ad78980a7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=693&q=80",
+        description: "business dress",
+        price: 160
+    },
+
+    {
+        id: 5,
+        image: "https://images.unsplash.com/photo-1604506272685-a999a4d122e7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=686&q=80",
+        description: "beach dress",
+        price: 110
+    }
+
+]
